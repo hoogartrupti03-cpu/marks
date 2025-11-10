a0e57761c8cf59c84c3c2d2306ca9ebd5be823b1
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2025-11-10 15:00:35 +0530

    Update marks.py

--- marks.py
+++ marks.py
@@ -15,11 +15,10 @@ def calc_grade(m1, m2, m3, m4, m5):
 
     print("Average Marks =", avg)
     print("Grade =", grade)
-
-m1 = float(input("Enter marks of subject 1: "))
-m2 = float(input("Enter marks of subject 2: "))
-m3 = float(input("Enter marks of subject 3: "))
-m4 = float(input("Enter marks of subject 4: "))
-m5 = float(input("Enter marks of subject 5: "))
+     m1 = float(input("Enter marks of subject 1: "))
+     m2 = float(input("Enter marks of subject 2: "))
+     m3 = float(input("Enter marks of subject 3: "))
+     m4 = float(input("Enter marks of subject 4: "))
+     m5 = float(input("Enter marks of subject 5: "))
 
 calc_grade(m1, m2, m3, m4, m5)
