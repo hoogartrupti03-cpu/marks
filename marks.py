c65f0209293c092e419cbb05f4ed4a44c448af4b
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2025-11-10 15:10:49 +0530

    Update marks.py

--- marks.py
+++ marks.py
@@ -1,23 +1,24 @@
+m1 = float(input("Enter marks of subject 1: "))
+m2 = float(input("Enter marks of subject 2: "))
+m3 = float(input("Enter marks of subject 3: "))
+m4 = float(input("Enter marks of subject 4: "))
+m5 = float(input("Enter marks of subject 5: "))
 
-def calc_grade(m1, m2, m3, m4, m5):
-    avg = (m1 + m2 + m3 + m4 + m5) / 5
 
-    if avg >= 90:
-        grade = "A"
-    elif avg >= 75:
-        grade = "B"
-    elif avg >= 60:
-        grade = "C"
-    elif avg >= 40:
-        grade = "D"
-    else:
-        grade = "Fail"
 
-    print("Average Marks =", avg)
+average = (m1 + m2 + m3 + m4 + m5) / 5
+
+
+
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+    print("Average Marks =", average)
     print("Grade =", grade)
-    m1 = float(input("Enter marks of subject 1: "))
-    m2 = float(input("Enter marks of subject 2: "))
-    m3 = float(input("Enter marks of subject 3: "))
-    m4 = float(input("Enter marks of subject 4: "))
-    m5 = float(input("Enter marks of subject 5: "))
-    calc_grade(m1, m2, m3, m4, m5)
